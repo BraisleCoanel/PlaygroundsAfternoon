f6147ec05d3af5172bd6aa8c51e67a2f4ab9f4d9
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-10-02 12:34:02 -0600

    Created 'ObjectsAndMethods' with a public claas.

--- PlaygroundsProject2.playground/Contents.swift
+++ PlaygroundsProject2.playground/Contents.swift
@@ -25,13 +25,13 @@ namingExample(outerName: "Branton")
 
 public func counting() -> Int
 {
-    return (7 * 1)
+    return (7 * 900)
 }
 print("I am not \(counting()) years old")
 
 if(counting() < 10)
 {
-    print("math works in swidt too")
+    print("math works in swift too")
 }
 else
 {
@@ -41,8 +41,8 @@ var test = 0
 
 while (test < counting())
 {
-    print("🤔")
+    print("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.")
     test += 1
 }
 
-print("xd")
+print("Done")
